388d4ee4d3ae20647403b6f8e0c0cc2c48dbea4f
Bruno Vinícius <brunovinicius082005@gmail.com>
2023-03-18 13:45:36 -0300

    Tipo dado Primitivo

---
+++ script.js
@@ -0,0 +1,9 @@
+//TIPO DE DADO PRIMITIVO
+
+const nome = 'Bruno'; //String
+const num1 = 10; //Int or Number
+const num2 = 10.52; //Float or Number
+let nomeAluno; //undefined -> não aponta pra local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponnta pra local nenhum na memória
+const aprovado = true; //Bollean = true, false (lógico)
+
